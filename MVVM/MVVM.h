db3810de61508cc1d296ceae1b2a7cfeefe24e56
Marko Hlebar <marko.hlebar@gmail.com>
2016-06-21 19:14:58 +0100

    Initial Commit

---
+++ MVVM/MVVM.h
@@ -0,0 +1,19 @@
+//
+//  MVVM.h
+//  MVVM
+//
+//  Created by Marko Hlebar on 21/06/2016.
+//  Copyright © 2016 Marko Hlebar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVVM.
+FOUNDATION_EXPORT double MVVMVersionNumber;
+
+//! Project version string for MVVM.
+FOUNDATION_EXPORT const unsigned char MVVMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVVM/PublicHeader.h>
+
+
